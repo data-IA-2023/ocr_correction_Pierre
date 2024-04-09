8881e22755e83faea13bde85fa6a45655ed48a96
PierreCav <cavallo.pierre@gmail.com>
2024-04-09 11:24:21 +0200

    ca marche

--- controler.py
+++ controler.py
@@ -7,12 +7,5 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
 
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse(
-        request=request, name="item.html", context={"id": id}
-    )
